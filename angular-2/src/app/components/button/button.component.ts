b9b71bb899b7c4c37b4573f5a9d6391f26ae1519
leticiareginajf <leticia.reginajf@gmail.com>
2024-05-24 15:41:37 -0300

    adicionando display e justifiy

--- angular-2/src/app/components/button/button.component.ts
+++ angular-2/src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -12,5 +12,17 @@ export class ButtonComponent {
 
   @Input({ required: true, alias: 'style'  }) 
   buttonStyle: 'white' | 'purple' = 'white';
+
+  @Input({ alias: 'disabled' })
+  isDisabled: boolean = true;
+
+
+  @Output('clicked') buttonClickedEmitt = new EventEmitter<void>();
+
+
+  onButtonClicked(){
+    this.buttonClickedEmitt.emit();
+
+  }
 
 }
